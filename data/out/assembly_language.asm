dfdccaee2de18cb0aa27d7979210d37c21d2598b
Secenary <45301338+Secenary@users.noreply.github.com>
2024-12-24 15:19:39 +0800

    Add files via upload

---
+++ data/out/assembly_language.asm
@@ -0,0 +1,13 @@
+.text
+    li t0, 8
+    li t1, 5
+    li t2, 3
+    sub t3, t2, t0
+    mv t4, t3
+    mul t5, t0, t1
+    addi t6, t1, 3
+    sub t1, t4, t0
+    mul t2, t6, t1
+    sub t3, t5, t2
+    mv t0, t3
+    mv a0, t0
